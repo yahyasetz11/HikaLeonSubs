4ef670ad62f40453979b6b4b22f9f5b0f5d96271
yahyasetz11 <yahyasetz@gmail.com>
2026-09-20 14:05:19 +0700

    Fix: tumbnail preview for episode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://hikaleonsubs.pages.dev',
   integrations: [tailwind()],
   output: 'static',
 });
